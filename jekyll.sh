376198f9069a16dbca6e5534889b0dd9be93fdff
Tom Readings <digitisatom@gmail.com>
2015-02-24 16:37:42 +0000

    format dates for use in jeyll posts

--- jekyll.sh
+++ jekyll.sh
@@ -1,17 +1,21 @@
 # define github repo path in .bash_profile
 
 # navigate to
-alias blog="cd $gitblog/_posts; subl .; git pull;"
-alias blogpost="cd $gitblog; ga; gc 'blog post update'; git push;"
-alias blogserve="cd $gitblog; open http://0.0.0.0:4000; bundle exec jekyll serve -w;"
+# alias blog="cd $gitblog/source/_posts; subl .; git pull;"
+# alias blogpost="cd $gitblog; ga; gc 'blog post update'; git push;"
 
+alias blog="cd $gitblog; git pull; cd $gitblog/source/_posts; subl .;"
+alias blogpost="cd $gitblog/public; ga; gc 'blog post update'; git push;"
+alias blogserve="cd $gitblog; open http://0.0.0.0:4000; bundle exec jekyll serve -w;"
+alias blogdate="date +\"%Y-%m-%d-T%H:%m:%S+00:00\""
+alias octodate="date +\"%Y-%m-%d-T%H:%m:%S+00:00\""
 
 newpost() {
   echo "new post"
   echo "Title:";
   read blogtitle;
   blogname=$(echo $blogtitle | tr " " -);
-  filename=$(date +"%Y-%m-%d-")$blogname".md";
+  filename=$gitblog/source/_posts/$(date +"%Y-%m-%d-")$blogname".md";
   # first arg is category, latter arguments as comma separated tags
   args=( "$@" );
   tags=$(printf ",%s" "${args[@]:1}");
