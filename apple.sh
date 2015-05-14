9cc9a6b3641599b363925546a4a3f848bda881ba
Thomas Readings <tombola@Thomass-MacBook-Pro.local>
2015-05-14 09:50:15 +0100

    apple specific command

---
+++ apple.sh
@@ -0,0 +1,4 @@
+alias showfiles="defaults write com.apple.finder AppleShowAllFiles YES"
+alias hidefiles="defaults write com.apple.finder AppleShowAllFiles NO"
+
+alias finderpathtitle="defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES" 
